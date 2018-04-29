2eaa1275adaf728b411affe68dd553eead6bde21
Nadia <nadia@192.168.1.21>
2018-04-29 21:28:38 +0200

    Sending emails

---
+++ StudentValidation/Models/EmailModel.cs
@@ -0,0 +1,1 @@
+﻿using System; using System.Collections.Generic; using System.Linq; using System.Web;  namespace StudentValidation.Models {     public class EmailModel     {          public string to { get; set; }         public string subject { get; set; }         public string body { get; set; }      } } 
